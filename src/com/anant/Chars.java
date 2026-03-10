0ebef87cd60186d316726cc58233b512675847d3
Anant <gigahello2021@gmail.com>
2026-03-10 15:45:02 +0000

    Added Fibonacci and Character files and covered Conditional Statements & Loops

---
+++ src/com/anant/Chars.java
@@ -0,0 +1,16 @@
+package com.anant;
+import java.util.Scanner;
+
+public class Chars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+//        System.out.println(ch);
+        if(ch >='a' && ch<='z'){
+            System.out.println("LowerCase");
+        }
+        else if(ch >='A' && ch<='Z'){
+            System.out.println("UpperCase");
+        }
+    }
+}
