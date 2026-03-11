fc81633b7c96c1cb60c3c150b6ef851d0d9e5050
Anant <gigahello2021@gmail.com>
2026-03-11 18:37:19 +0000

    Added for each loop concept file

---
+++ Arrays/src/com/anant/forEach.java
@@ -0,0 +1,17 @@
+package com.anant;
+import java.util.Scanner;
+public class forEach {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] n = new int[5];
+        for (int i = 0; i <n.length ; i++) {
+            System.out.print("Enter number: ");
+            n[i] = sc.nextInt();
+        }
+
+        // for each loop
+        for(int num:n){
+            System.out.print(num+ " ");
+        }
+    }
+}
